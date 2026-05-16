bab3b00d8ee7ef0b88d3a8744ba26ffcd882775d
symbiontarch <symbiontarch@users.noreply.github.com>
2026-05-15 19:37:23 -0600

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === "github-pages" ? "/morphon-landing/" : "/",
   plugins: [react()],
-});
+}));

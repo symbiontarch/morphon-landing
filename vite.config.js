eefd774c66399d2aad155f684c919a76504687ba
Victor <amra.b9@gmail.com>
2026-06-03 15:43:40 -0600

    Fix Pages asset base for custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "github-pages" ? "/morphon-landing/" : "/",
+export default defineConfig({
+  base: "/",
   plugins: [react()],
-}));
+});
